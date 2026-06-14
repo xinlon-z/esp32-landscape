9a33b84ed090f5e039b3b837306e94fa3bb3b2f0
Xinzheng Long <xinzheng.long@outlook.com>
2026-06-14 13:55:59 +0800

    Update glyph mask threshold test

--- tests/test_clock_glyph_mask.cpp
+++ tests/test_clock_glyph_mask.cpp
@@ -5,8 +5,9 @@
 TEST(ClockGlyphMask, RemovesOnlyVeryLowAlphaEdgeNoise)
 {
     EXPECT_EQ(clockGlyphMaskOpacity(0), 0);
-    EXPECT_EQ(clockGlyphMaskOpacity(31), 0);
-    EXPECT_EQ(clockGlyphMaskOpacity(32), 32);
+    EXPECT_EQ(clockGlyphMaskOpacity(3), 0);
+    EXPECT_EQ(clockGlyphMaskOpacity(4), 4);
+    EXPECT_EQ(clockGlyphMaskOpacity(31), 31);
     EXPECT_EQ(clockGlyphMaskOpacity(128), 128);
     EXPECT_EQ(clockGlyphMaskOpacity(255), 255);
 }
